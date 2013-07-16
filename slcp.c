5e582b9ca702a7e05ed09b3af67f2abb41f549c3
Markus Teich <markus.teich@stusta.mhn.de>
2013-07-17 00:44:16 +0200

    put license to its own file.

--- slcp.c
+++ slcp.c
@@ -1,11 +1,5 @@
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <teichm@in.tum.de> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return Markus Teich
- * ----------------------------------------------------------------------------
- */
+// slcp - suckless C prompt
+// See LICENSE file for copyright and license details.
 
 #include <buffer.h>
 #include <git2.h>
